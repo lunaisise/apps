a4835a6015e8cfe9a5645c2e330385fc660d1fc1
Lunaisise <lunaisise@hotmail.co.jp>
2022-08-20 21:42:14 +0900

    Dompisha: test

---
+++ dompisha/js/index.js
@@ -0,0 +1,20 @@
+window.addEventListener('DOMContentLoaded', () => {
+    async function startCamera() {
+        try {
+            const stream = await navigator.mediaDevices.getUserMedia({
+                video: {
+                    width: 1280,
+                    height: 720,
+                    facingMode: {
+                        // exact: 'environment'
+                    }
+                },
+                audio: false
+            });
+            document.getElementById('video');
+        } catch (e) {
+            console.log(e);
+        }
+    }
+    startCamera();
+});
